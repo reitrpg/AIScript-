581e9419ab530ba9e5d4ce217e79f7bb1f963721
reitrpg <rei.you2.11@gmail.com>
2026-07-31 21:50:32 +0900

    js/tree/DeathTree.js の作成

---
+++ js/tree/DeathTree.js
@@ -0,0 +1,232 @@
+/**
+ * World Creator
+ * Death Tree
+ *
+ * 死・終焉系成長ツリー管理
+ */
+
+
+import treeData from "../data/treeData.js";
+import BigNumber from "../number/BigNumber.js";
+import eventBus from "../core/eventBus.js";
+
+
+class DeathTree {
+
+
+    constructor() {
+
+        this.nodes = new Map();
+
+        this.unlocked = new Set();
+
+    }
+
+
+
+    /**
+     * 初期化
+     */
+
+    init() {
+
+
+        const data =
+            treeData.death.nodes;
+
+
+        for (const id in data) {
+
+
+            const node =
+                data[id];
+
+
+            this.nodes.set(
+                id,
+                {
+
+                    ...node,
+
+                    cost:
+                        this.convertCost(
+                            node.cost
+                        )
+
+                }
+            );
+
+        }
+
+    }
+
+
+
+    /**
+     * コスト変換
+     */
+
+    convertCost(cost) {
+
+
+        const result = {};
+
+
+        for (const key in cost) {
+
+
+            result[key] =
+                BigNumber.from(
+                    cost[key]
+                );
+
+        }
+
+
+        return result;
+
+    }
+
+
+
+    /**
+     * ノード取得
+     */
+
+    get(id) {
+
+        return this.nodes.get(id);
+
+    }
+
+
+
+    /**
+     * 解放確認
+     */
+
+    isUnlocked(id) {
+
+        return this.unlocked.has(id);
+
+    }
+
+
+
+    /**
+     * 解放
+     */
+
+    unlock(id) {
+
+
+        if (!this.nodes.has(id)) {
+
+            return false;
+
+        }
+
+
+        if (
+            this.isUnlocked(id)
+        ) {
+
+            return false;
+
+        }
+
+
+        this.unlocked.add(id);
+
+
+        eventBus.emit(
+            "tree:deathUnlocked",
+            id
+        );
+
+
+        return true;
+
+    }
+
+
+
+    /**
+     * 解放済み一覧
+     */
+
+    getUnlocked() {
+
+        return [
+            ...this.unlocked
+        ];
+
+    }
+
+
+
+    /**
+     * 全ノード取得
+     */
+
+    getAll() {
+
+        return this.nodes;
+
+    }
+
+
+
+    /**
+     * 保存
+     */
+
+    toJSON() {
+
+
+        return {
+
+            unlocked:
+                [
+                    ...this.unlocked
+                ]
+
+        };
+
+    }
+
+
+
+    /**
+     * 復元
+     */
+
+    load(data) {
+
+
+        if (!data) {
+
+            return;
+
+        }
+
+
+        this.unlocked =
+            new Set(
+                data.unlocked ?? []
+            );
+
+    }
+
+
+}
+
+
+const deathTree =
+    new DeathTree();
+
+
+deathTree.init();
+
+
+export default deathTree;
