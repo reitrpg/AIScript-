d07c6540a3d0290091afd97d1c918744e1bf87d1
reitrpg <rei.you2.11@gmail.com>
2026-07-31 22:12:12 +0900

    service-worker.js の更新

--- service-worker.js
+++ service-worker.js
@@ -1,77 +1,172 @@
-const CACHE_NAME = "world-creator-v1";
+/**
+ * World Creator
+ * Service Worker
+ *
+ * PWA Cache Management
+ */
+
+
+const CACHE_NAME =
+    "world-creator-v1";
+
+
 
 const CACHE_FILES = [
+
     "./",
+
     "./index.html",
+
     "./manifest.json",
 
     "./css/style.css",
-    "./css/mobile.css",
 
-    "./js/core/main.js"
+    "./css/mobile.css"
+
 ];
 
 
-/**
- * Install
- */
-self.addEventListener("install", (event) => {
-
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(CACHE_FILES);
-            })
-    );
-
-    self.skipWaiting();
-});
-
 
 /**
- * Activate
+ * インストール
  */
-self.addEventListener("activate", (event) => {
 
-    event.waitUntil(
-        caches.keys()
-            .then((keys) => {
+self.addEventListener(
 
-                return Promise.all(
-                    keys.map((key) => {
+    "install",
 
-                        if (key !== CACHE_NAME) {
-                            return caches.delete(key);
-                        }
-
-                    })
-                );
-
-            })
-    );
-
-    self.clients.claim();
-});
+    event => {
 
 
-/**
- * Fetch
- */
-self.addEventListener("fetch", (event) => {
+        event.waitUntil(
 
-    event.respondWith(
+            caches.open(
+                CACHE_NAME
+            )
+            .then(
 
-        caches.match(event.request)
-            .then((response) => {
+                cache => {
 
-                if (response) {
-                    return response;
+                    return cache.addAll(
+                        CACHE_FILES
+                    );
+
                 }
 
-                return fetch(event.request);
+            )
 
-            })
+        );
 
-    );
 
-});
+    }
+
+);
+
+
+
+
+
+/**
+ * 起動
+ */
+
+self.addEventListener(
+
+    "activate",
+
+    event => {
+
+
+        event.waitUntil(
+
+            caches.keys()
+
+            .then(
+
+                keys => {
+
+
+                    return Promise.all(
+
+                        keys.map(
+
+                            key => {
+
+
+                                if (
+                                    key !== CACHE_NAME
+                                ) {
+
+                                    return caches.delete(
+                                        key
+                                    );
+
+                                }
+
+
+                            }
+
+                        )
+
+                    );
+
+
+                }
+
+            )
+
+        );
+
+
+    }
+
+);
+
+
+
+
+
+/**
+ * リクエスト処理
+ */
+
+self.addEventListener(
+
+    "fetch",
+
+    event => {
+
+
+        event.respondWith(
+
+            caches.match(
+                event.request
+            )
+            .then(
+
+                response => {
+
+
+                    return (
+
+                        response
+
+                        ||
+
+                        fetch(
+                            event.request
+                        )
+
+                    );
+
+
+                }
+
+            )
+
+        );
+
+
+    }
+
+);
