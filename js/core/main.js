37d802d11f1d1f00dcb346cecd51bbe0ab48ef1a
reitrpg <rei.you2.11@gmail.com>
2026-08-01 12:05:13 +0900

    main.js の更新

--- js/core/main.js
+++ js/core/main.js
@@ -2,294 +2,79 @@
  * World Creator
  * Main Entry
  *
- * Debug Initialization Version
+ * Game Initialize
  */
 
 
-import game from "./game.js";
+import Game from "./game.js";
 
 import SaveManager from "./save.js";
-
-import WorldManager from "../world/Manager.js";
-
-import ResourceManager from "../resource/Manager.js";
-
-import ResearchManager from "../research/Manager.js";
-
-import Converter from "../converter/Converter.js";
 
 import UI from "../ui/UI.js";
 
 import ResearchUI from "../ui/ResearchUI.js";
-
-import ConverterUI from "../ui/ConverterUI.js";
-
-import eventBus from "./eventBus.js";
 
 
 
 class Main {
 
 
-    start(){
+    constructor(){
 
 
-        console.log(
+        this.initialized=false;
 
-            "Main start"
+
+    }
+
+
+
+    init(){
+
+
+        if(this.initialized){
+
+
+            return;
+
+
+        }
+
+
+
+        this.initialized=true;
+
+
+
+        SaveManager.load();
+
+
+
+        UI.init(
+
+            "game"
 
         );
 
 
 
-        try{
+        ResearchUI.init(
 
+            "research"
 
-            console.log(
+        );
 
-                "Resource init"
 
-            );
 
-
-            ResourceManager.init();
-
-
-
-            console.log(
-
-                "Save load"
-
-            );
-
-
-            const loaded=
-
-            SaveManager.load();
-
-
-
-            console.log(
-
-                "Loaded:",
-
-                loaded
-
-            );
-
-
-
-            if(
-
-                !loaded
-
-                ||
-
-                !WorldManager.getCurrent()
-
-            ){
-
-
-                console.log(
-
-                    "Create World"
-
-                );
-
-
-
-                WorldManager.createWorld();
-
-
-            }
-
-
-
-            console.log(
-
-                "Resource sync"
-
-            );
-
-
-            ResourceManager.syncWorldResources();
-
-
-
-            console.log(
-
-                "Converter init"
-
-            );
-
-
-            Converter.setResearchMultiplier(
-
-                ResearchManager
-
-                .getConverterMultiplier()
-
-            );
-
-
-
-            console.log(
-
-                "UI init"
-
-            );
-
-
-            UI.init(
-
-                "app"
-
-            );
-
-
-
-            ResearchUI.init(
-
-                "research-area"
-
-            );
-
-
-
-            ConverterUI.init(
-
-                "converter-area"
-
-            );
-
-
-
-            console.log(
-
-                "Event bind"
-
-            );
-
-
-
-            eventBus.on(
-
-                "research:update",
-
-                ()=>{
-
-
-                    Converter.setResearchMultiplier(
-
-                        ResearchManager
-
-                        .getConverterMultiplier()
-
-                    );
-
-
-                    SaveManager.save();
-
-
-                }
-
-            );
-
-
-
-            eventBus.on(
-
-                "resource:update",
-
-                ()=>{
-
-
-                    SaveManager.save();
-
-
-                }
-
-            );
-
-
-
-            eventBus.on(
-
-                "world:update",
-
-                ()=>{
-
-
-                    SaveManager.save();
-
-
-                }
-
-            );
-
-
-
-            eventBus.on(
-
-                "world:rebirth",
-
-                ()=>{
-
-
-                    ResourceManager.syncWorldResources();
-
-
-                    SaveManager.save();
-
-
-                }
-
-            );
-
-
-
-            console.log(
-
-                "Game start"
-
-            );
-
-
-            game.start();
-
-
-
-            console.log(
-
-                "World Creator Started"
-
-            );
-
-
-        }
-
-        catch(error){
-
-
-            console.error(
-
-                "Initialization Error",
-
-                error
-
-            );
-
-
-        }
+        Game.start();
 
 
     }
+
 
 
 }
 
 
 
-new Main()
-
-.start();
+export default new Main();
