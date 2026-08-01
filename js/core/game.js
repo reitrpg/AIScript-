c3f5f902fbce559bb8e3dbd93d4a397d81236c8f
reitrpg <rei.you2.11@gmail.com>
2026-08-01 09:13:30 +0900

    game.js の更新

--- js/core/game.js
+++ js/core/game.js
@@ -1,334 +1,191 @@
-/**
- * World Creator
- * Game Core
- *
- * Research Connect Version
- */
-
-
 import eventBus from "./eventBus.js";
 
 import time from "./time.js";
 
 import save from "./save.js";
 
-
 import ResourceManager from "../resource/Manager.js";
 
 import WorldManager from "../world/Manager.js";
-
-import Converter from "../resource/Converter.js";
 
 import ResearchManager from "../research/Manager.js";
 
 
 
-class Game {
+class Game{
 
 
-    constructor() {
+constructor(){
 
+this.running=false;
 
-        this.running = false;
+}
 
 
-        this.initialized = false;
 
+init(){
 
-    }
+ResourceManager.init();
 
+WorldManager.init();
 
+ResearchManager.init();
 
-    init() {
 
+eventBus.on(
 
-        if (
+"time:tick",
 
-            this.initialized
+()=>{
 
-        ) {
+this.update();
 
+}
 
-            return;
+);
 
 
-        }
 
+eventBus.on(
 
+"game:save",
 
-        ResourceManager.init();
+()=>{
 
+this.save();
 
-        WorldManager.init();
+}
 
-
-        ResearchManager.init();
-
-
-
-        this.bind();
-
-
-
-        this.initialized = true;
-
-
-    }
-
-
-
-    bind() {
-
-
-        eventBus.on(
-
-            "time:tick",
-
-            () => {
-
-
-                this.update();
-
-
-            }
-
-        );
-
-
-
-        eventBus.on(
-
-            "game:save",
-
-            () => {
-
-
-                this.save();
-
-
-            }
-
-        );
-
-
-    }
-
-
-
-    start() {
-
-
-        if (
-
-            this.running
-
-        ) {
-
-
-            return;
-
-
-        }
-
-
-
-        this.running = true;
-
-
-        time.start();
-
-
-    }
-
-
-
-    stop() {
-
-
-        this.running = false;
-
-
-        time.stop();
-
-
-        this.save();
-
-
-    }
-
-
-
-    update() {
-
-
-        if (
-
-            !this.running
-
-        ) {
-
-
-            return;
-
-
-        }
-
-
-
-        /*
-            資源生産
-        */
-
-
-        ResourceManager.add(
-
-            "wood",
-
-            1
-
-        );
-
-
-        ResourceManager.add(
-
-            "stone",
-
-            1
-
-        );
-
-
-
-        Converter.tick();
-
-
-
-        /*
-            研究ポイント
-        */
-
-
-        ResearchManager.addPoint(
-
-            1
-
-        );
-
-
-
-        WorldManager.update();
-
-
-
-        eventBus.emit(
-
-            "game:update"
-
-        );
-
-
-    }
-
-
-
-    save() {
-
-
-        save.save({
-
-
-            resources:
-
-                ResourceManager.toJSON(),
-
-
-
-            world:
-
-                WorldManager.toJSON(),
-
-
-
-            research:
-
-                ResearchManager.toJSON(),
-
-
-
-            time:
-
-                time.toJSON()
-
-
-
-        });
-
-
-    }
-
-
-
-    load() {
-
-
-        const data =
-
-            save.load();
-
-
-
-        if (!data) {
-
-
-            return;
-
-
-        }
-
-
-
-        ResourceManager.load(
-
-            data.resources
-
-        );
-
-
-
-        WorldManager.load(
-
-            data.world
-
-        );
-
-
-
-        ResearchManager.load(
-
-            data.research
-
-        );
-
-
-
-        time.load(
-
-            data.time
-
-        );
-
-
-    }
+);
 
 
 }
 
 
 
-const game =
+start(){
 
-    new Game();
+if(this.running){
 
+return;
+
+}
+
+
+this.running=true;
+
+time.start();
+
+
+}
+
+
+
+update(){
+
+
+ResourceManager.update();
+
+
+ResearchManager.addPoint(1);
+
+
+WorldManager.update();
+
+
+eventBus.emit(
+
+"game:update"
+
+);
+
+
+}
+
+
+
+save(){
+
+
+save.save({
+
+resource:
+
+ResourceManager.toJSON(),
+
+
+world:
+
+WorldManager.toJSON(),
+
+
+research:
+
+ResearchManager.toJSON(),
+
+
+time:
+
+time.toJSON()
+
+});
+
+
+}
+
+
+
+load(){
+
+const data=
+
+save.load();
+
+
+if(!data){
+
+return;
+
+}
+
+
+ResourceManager.load(
+
+data.resource
+
+);
+
+
+WorldManager.load(
+
+data.world
+
+);
+
+
+ResearchManager.load(
+
+data.research
+
+);
+
+
+time.load(
+
+data.time
+
+);
+
+
+}
+
+
+
+}
+
+
+
+const game=new Game();
 
 
 export default game;
