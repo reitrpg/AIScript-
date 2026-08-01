e3d73eebdf7ec968e31a95451570430b8af6937b
reitrpg <rei.you2.11@gmail.com>
2026-08-01 09:13:58 +0900

    BigNumber.js の更新

--- js/number/BigNumber.js
+++ js/number/BigNumber.js
@@ -1,215 +1,56 @@
-/**
- * World Creator
- * BigNumber System
- *
- * 巨大数管理
- */
+class BigNumber{
 
 
-class BigNumber {
+constructor(value=0){
 
+this.value=Number(value);
 
-    constructor(
-        value = 0,
-        exponent = 0
-    ) {
+}
 
 
-        this.value = Number(value);
 
-        this.exponent = Number(exponent);
+add(value){
 
+this.value+=Number(value);
 
-        this.normalize();
+}
 
 
-    }
 
+subtract(value){
 
+this.value-=Number(value);
 
-    /**
-     * 生成
-     */
+}
 
-    static from(value) {
 
 
-        if (
-            value instanceof BigNumber
-        ) {
+toString(){
 
+return this.value.toString();
 
-            return new BigNumber(
+}
 
-                value.value,
 
-                value.exponent
 
-            );
+toJSON(){
 
+return this.value;
 
-        }
+}
 
 
 
-        if (
-            typeof value === "object"
-        ) {
+static from(value){
 
+return new BigNumber(value);
 
-            return new BigNumber(
+}
 
-                value.value,
 
-                value.exponent
 
-            );
+}
 
 
-        }
 
-
-
-        return new BigNumber(
-
-            value,
-
-            0
-
-        );
-
-
-    }
-
-
-
-    /**
-     * 正規化
-     */
-
-    normalize() {
-
-
-        if (
-            this.value === 0
-        ) {
-
-
-            this.exponent = 0;
-
-
-            return;
-
-
-        }
-
-
-
-        while (
-
-            Math.abs(this.value) >= 10
-
-        ) {
-
-
-            this.value /= 10;
-
-
-            this.exponent++;
-
-
-        }
-
-
-
-        while (
-
-            Math.abs(this.value) < 1
-
-            &&
-
-            this.value !== 0
-
-        ) {
-
-
-            this.value *= 10;
-
-
-            this.exponent--;
-
-
-        }
-
-
-    }
-
-
-
-    /**
-     * 加算
-     */
-
-    add(value) {
-
-
-        const target =
-
-            BigNumber.from(
-                value
-            );
-
-
-
-        if (
-
-            target.exponent ===
-
-            this.exponent
-
-        ) {
-
-
-            this.value +=
-
-                target.value;
-
-
-        }
-
-        else {
-
-
-            const diff =
-
-                this.exponent
-
-                -
-
-                target.exponent;
-
-
-
-            if (
-                diff > 15
-            ) {
-
-                return this;
-
-            }
-
-
-
-            if (
-                diff < -15
-            ) {
-
-
-                this.value =
-
-                    target.value;
-
-
-                this.exponent =
-
-                    target
+export default BigNumber;
