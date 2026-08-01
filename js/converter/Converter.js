5c0ce2038be9c922a7ddae8910da5be374b87770
reitrpg <rei.you2.11@gmail.com>
2026-08-01 11:16:23 +0900

    js/converter/Converter.js の作成

---
+++ js/converter/Converter.js
@@ -0,0 +1,224 @@
+/**
+ * World Creator
+ * Resource Converter
+ *
+ * Resource Processing System
+ */
+
+
+import ResourceManager from "../resource/Manager.js";
+
+import eventBus from "../core/eventBus.js";
+
+
+
+class Converter {
+
+
+    constructor(){
+
+
+        this.recipes={
+
+
+            wood_food:{
+
+
+                name:"木材加工食料化",
+
+
+                input:"wood",
+
+
+                inputAmount:10,
+
+
+                output:"food",
+
+
+                outputAmount:5
+
+
+            },
+
+
+
+            stone_ore:{
+
+
+                name:"石材精錬",
+
+
+                input:"stone",
+
+
+                inputAmount:20,
+
+
+                output:"ore",
+
+
+                outputAmount:5
+
+
+            },
+
+
+
+            mana_crystal:{
+
+
+                name:"魔力結晶生成",
+
+
+                input:"mana",
+
+
+                inputAmount:50,
+
+
+                output:"crystal",
+
+
+                outputAmount:1
+
+
+            }
+
+
+        };
+
+
+    }
+
+
+
+    getAll(){
+
+
+        return this.recipes;
+
+
+    }
+
+
+
+    convert(id){
+
+
+        const recipe=
+
+        this.recipes[id];
+
+
+
+        if(!recipe){
+
+
+            return false;
+
+        }
+
+
+
+        const input=
+
+        ResourceManager.get(
+
+            recipe.input
+
+        );
+
+
+
+        if(!input){
+
+
+            return false;
+
+        }
+
+
+
+        if(
+
+            !input.consume(
+
+                recipe.inputAmount
+
+            )
+
+        ){
+
+
+            return false;
+
+        }
+
+
+
+        let output=
+
+        ResourceManager.get(
+
+            recipe.output
+
+        );
+
+
+
+        if(!output){
+
+
+            ResourceManager.create(
+
+                recipe.output,
+
+                recipe.output,
+
+                0
+
+            );
+
+
+            output=
+
+            ResourceManager.get(
+
+                recipe.output
+
+            );
+
+
+        }
+
+
+
+        output.add(
+
+            recipe.outputAmount
+
+        );
+
+
+
+        eventBus.emit(
+
+            "resource:update"
+
+        );
+
+
+
+        return true;
+
+
+    }
+
+
+
+}
+
+
+
+export default new Converter();
