21b655e006e6210f9a0130ca1209911fc70c0c89
reitrpg <rei.you2.11@gmail.com>
2026-08-01 09:15:48 +0900

    Manager.js の更新

--- js/world/Manager.js
+++ js/world/Manager.js
@@ -1,363 +1,114 @@
-/**
- * World Creator
- * World Manager
- *
- * World Create Check Version
- */
-
-
-import BigNumber from "../number/BigNumber.js";
-
 import eventBus from "../core/eventBus.js";
 
 
+class WorldManager{
 
-class WorldManager {
 
+constructor(){
 
-    constructor() {
+this.current=null;
 
+}
 
-        this.worlds = [];
 
 
-        this.current = null;
+init(){
 
+}
 
-        this.initialized = false;
 
 
-    }
+createWorld(){
 
 
+this.current={
 
-    init() {
 
+id:
+Date.now(),
 
-        if (
 
-            this.initialized
+level:1,
 
-        ) {
 
+age:0
 
-            return;
 
+};
 
-        }
 
 
+eventBus.emit(
 
-        this.initialized = true;
+"world:created"
 
-
-
-        console.log(
-
-            "[World] Ready"
-
-        );
-
-
-    }
-
-
-
-    createWorld() {
-
-
-        const world = {
-
-
-            id:
-
-                Date.now()
-
-                .toString(),
-
-
-
-            level:
-
-                1,
-
-
-
-            age:
-
-                BigNumber.from(
-
-                    0
-
-                ),
-
-
-
-            population:
-
-                10,
-
-
-
-            created:
-
-                Date.now()
-
-
-        };
-
-
-
-        this.worlds.push(
-
-            world
-
-        );
-
-
-
-        this.current =
-
-            world;
-
-
-
-        console.log(
-
-            "[World] Created",
-
-            world.id
-
-        );
-
-
-
-        eventBus.emit(
-
-            "world:created",
-
-            world
-
-        );
-
-
-
-        return world;
-
-
-    }
-
-
-
-    update() {
-
-
-        if (
-
-            !this.current
-
-        ) {
-
-
-            return;
-
-
-        }
-
-
-
-        this.current.age.add(
-
-            1
-
-        );
-
-
-
-        eventBus.emit(
-
-            "world:update",
-
-            this.current
-
-        );
-
-
-    }
-
-
-
-    getCurrent() {
-
-
-        return this.current;
-
-
-    }
-
-
-
-    toJSON() {
-
-
-        return {
-
-
-            worlds:
-
-                this.worlds.map(
-
-                    world => ({
-
-
-                        id:
-
-                            world.id,
-
-
-
-                        level:
-
-                            world.level,
-
-
-
-                        age:
-
-                            world.age.toJSON(),
-
-
-
-                        population:
-
-                            world.population,
-
-
-
-                        created:
-
-                            world.created
-
-
-                    })
-
-                ),
-
-
-
-            current:
-
-                this.current
-
-                ?
-
-                this.current.id
-
-                :
-
-                null
-
-
-        };
-
-
-    }
-
-
-
-    load(data) {
-
-
-        if (
-
-            !data
-
-        ) {
-
-
-            return;
-
-
-        }
-
-
-
-        this.worlds =
-
-            data.worlds
-
-            ?
-
-            data.worlds.map(
-
-                world => ({
-
-
-                    ...world,
-
-
-                    age:
-
-                        BigNumber.from(
-
-                            world.age
-
-                        )
-
-
-                })
-
-            )
-
-            :
-
-            [];
-
-
-
-        this.current =
-
-
-            this.worlds.find(
-
-                world =>
-
-                    world.id ===
-
-                    data.current
-
-
-            )
-
-            ||
-
-            null;
-
-
-    }
+);
 
 
 }
 
 
 
-const worldManager =
+update(){
 
-    new WorldManager();
 
+if(!this.current){
+
+return;
+
+}
+
+
+this.current.age++;
+
+
+}
+
+
+
+getCurrent(){
+
+return this.current;
+
+}
+
+
+
+toJSON(){
+
+return this.current;
+
+}
+
+
+
+load(data){
+
+this.current=data;
+
+
+}
+
+
+
+}
+
+
+
+const worldManager=
+
+new WorldManager();
 
 
 eventBus.on(
 
-    "world:create",
+"world:create",
 
-    () => {
+()=>{
 
+worldManager.createWorld();
 
-        worldManager.createWorld();
-
-
-    }
+}
 
 );
 
