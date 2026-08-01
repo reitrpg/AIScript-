b6869403ba1356122961aef4959bd416512de643
reitrpg <rei.you2.11@gmail.com>
2026-08-01 09:22:42 +0900

    Tabs.js の更新

--- js/ui/Tabs.js
+++ js/ui/Tabs.js
@@ -1,10 +1,82 @@
-class Tabs{
+class Tabs {
 
 
-init(){
+    init(){
 
-}
 
+        const buttons =
+
+            document.querySelectorAll(
+                ".tabs button"
+            );
+
+
+
+        const sections = [
+
+            document.querySelector(
+                "#world-info"
+            ),
+
+            document.querySelector(
+                "#resource-list"
+            ),
+
+            document.querySelector(
+                "#research-list"
+            )
+
+        ];
+
+
+
+        buttons.forEach(
+
+            (button,index)=>{
+
+
+                button.onclick = ()=>{
+
+
+                    sections.forEach(
+
+                        section=>{
+
+
+                            if(section){
+
+                                section.parentElement
+                                .style.display="none";
+
+                            }
+
+
+                        }
+
+                    );
+
+
+
+                    if(sections[index]){
+
+
+                        sections[index]
+                        .parentElement
+                        .style.display="block";
+
+
+                    }
+
+
+                };
+
+
+            }
+
+        );
+
+
+    }
 
 
 }
