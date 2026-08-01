5fe6c57af8ee0c609ed1aa0c44c88d94ce410533
reitrpg <rei.you2.11@gmail.com>
2026-08-01 09:17:00 +0900

    Research.js の更新

--- js/ui/Research.js
+++ js/ui/Research.js
@@ -1,274 +1,156 @@
-/**
- * World Creator
- * Research UI Controller
- *
- * UI統合版
- */
-
-
-import eventBus from "../core/eventBus.js";
-
 import ResearchManager from "../research/Manager.js";
 
 
 
-class ResearchUI {
+class ResearchUI{
 
 
-    constructor() {
+init(){
 
 
-        this.initialized = false;
-
-
-    }
-
-
-
-    init() {
-
-
-        if (
-
-            this.initialized
-
-        ) {
-
-
-            return;
-
-
-        }
-
-
-
-        this.bind();
-
-
-
-        this.initialized = true;
-
-
-    }
-
-
-
-    bind() {
-
-
-        eventBus.on(
-
-            "research:update",
-
-            () => {
-
-
-                this.update();
-
-
-            }
-
-        );
-
-
-
-        eventBus.on(
-
-            "research:unlock",
-
-            () => {
-
-
-                this.update();
-
-
-            }
-
-        );
-
-
-    }
-
-
-
-    update() {
-
-
-        this.updatePoint();
-
-
-        this.updateList();
-
-
-    }
-
-
-
-    updatePoint() {
-
-
-        const area =
-
-            document.getElementById(
-
-                "research-point"
-
-            );
-
-
-
-        if (!area) {
-
-
-            return;
-
-
-        }
-
-
-
-        area.textContent =
-
-
-            "Research Point : "
-
-            +
-
-            ResearchManager.points;
-
-
-    }
-
-
-
-    updateList() {
-
-
-        const area =
-
-            document.getElementById(
-
-                "research-list"
-
-            );
-
-
-
-        if (!area) {
-
-
-            return;
-
-
-        }
-
-
-
-        area.innerHTML = "";
-
-
-
-        const researches =
-
-            ResearchManager.getAll();
-
-
-
-        for (
-
-            const id in researches
-
-        ) {
-
-
-            const research =
-
-                researches[id];
-
-
-
-            const button =
-
-                document.createElement(
-
-                    "button"
-
-                );
-
-
-
-            button.textContent =
-
-
-                research.name
-
-                +
-
-                " "
-
-                +
-
-                (
-
-                    research.unlocked
-
-                    ?
-
-                    "Unlocked"
-
-                    :
-
-                    "Cost "
-
-                    +
-
-                    research.cost
-
-                );
-
-
-
-            button.disabled =
-
-                research.unlocked;
-
-
-
-            button.onclick = () => {
-
-
-                ResearchManager.unlock(
-
-                    id
-
-                );
-
-
-            };
-
-
-
-            area.appendChild(
-
-                button
-
-            );
-
-
-        }
-
-
-    }
+this.update();
 
 
 }
 
 
 
-const researchUI =
+update(){
 
-    new ResearchUI();
 
+const point=
+
+document.getElementById(
+
+"research-point"
+
+);
+
+
+
+if(point){
+
+point.textContent=
+
+"Research Point : "
+
++
+
+ResearchManager.points;
+
+}
+
+
+
+const list=
+
+document.getElementById(
+
+"research-list"
+
+);
+
+
+
+if(!list){
+
+return;
+
+}
+
+
+
+list.innerHTML="";
+
+
+
+const data=
+
+ResearchManager.getAll();
+
+
+
+for(
+
+const id in data
+
+){
+
+
+const item=data[id];
+
+
+
+const button=
+
+document.createElement(
+
+"button"
+
+);
+
+
+
+button.textContent=
+
+item.name
+
++
+
+" "
+
++
+
+(
+
+item.unlocked
+
+?
+
+"Unlocked"
+
+:
+
+"Cost "
+
++
+
+item.cost
+
+);
+
+
+
+button.onclick=()=>{
+
+
+ResearchManager.unlock(id);
+
+
+this.update();
+
+
+};
+
+
+
+list.appendChild(button);
+
+
+}
+
+
+
+}
+
+
+}
+
+
+
+const researchUI=
+
+new ResearchUI();
 
 
 export default researchUI;
