cd38b73d5b462ae127137877a4da98ce6c67e8b5
reitrpg <rei.you2.11@gmail.com>
2026-08-01 11:23:24 +0900

    Research.js の更新

--- js/ui/Research.js
+++ js/ui/Research.js
@@ -1,71 +1,181 @@
 /**
  * World Creator
- * Research UI
+ * Research Data
  *
- * Integrated Version
+ * Individual Research Object
  */
 
 
-import eventBus from "../core/eventBus.js";
-
-import ResearchManager from "../research/Manager.js";
+import BigNumber from "../number/BigNumber.js";
 
 
 
-class ResearchUI {
+class Research {
 
 
-    constructor(){
+    constructor(data={}){
 
 
-        this.initialized = false;
+        this.id=
+
+        data.id
+
+        ??
+
+        "unknown";
+
+
+
+        this.name=
+
+        data.name
+
+        ??
+
+        "Research";
+
+
+
+        this.description=
+
+        data.description
+
+        ??
+
+        "";
+
+
+
+        this.level=
+
+        data.level
+
+        ??
+
+        0;
+
+
+
+        this.max=
+
+        data.max
+
+        ??
+
+        1;
+
+
+
+        this.effect=
+
+        data.effect
+
+        ??
+
+        1;
+
+
+
+        this.type=
+
+        data.type
+
+        ??
+
+        "production";
+
+
+
+        this.cost=
+
+        data.cost
+
+        ??
+
+        {};
+
+
+
+        this.progress=
+
+        BigNumber.zero();
+
+
+
+        this.completed=false;
 
 
     }
 
 
 
-    init(){
+    addProgress(value){
 
 
-        if(this.initialized){
+        if(
+
+            this.level >= this.max
+
+        ){
 
 
             return;
-
 
         }
 
 
 
-        this.bind();
+        this.progress.add(
+
+            value
+
+        );
 
 
-        this.update();
+
+        if(
+
+            this.checkComplete()
+
+        ){
 
 
+            this.complete();
 
-        this.initialized = true;
+
+        }
 
 
     }
 
 
 
-    bind(){
+    checkComplete(){
 
 
-        eventBus.on(
+        const need=
 
-            "research:update",
+        BigNumber.from(
 
-            () => {
+            100
+
+            *
+
+            (
+
+                this.level+1
+
+            )
+
+        );
 
 
-                this.update();
 
+        return this.progress
 
-            }
+        .greaterOrEqual(
+
+            need
 
         );
 
@@ -74,194 +184,145 @@ class ResearchUI {
 
 
 
-    update(){
+    complete(){
 
 
-        this.updatePoint();
+        if(
 
-
-        this.updateList();
-
-
-    }
-
-
-
-    updatePoint(){
-
-
-        const area =
-
-            document.getElementById(
-
-                "research-point"
-
-            );
-
-
-
-        if(!area){
-
-
-            return;
-
-
-        }
-
-
-
-        area.textContent =
-
-
-            "Research Point : "
-
-            +
-
-            ResearchManager.points;
-
-
-    }
-
-
-
-    updateList(){
-
-
-        const area =
-
-            document.getElementById(
-
-                "research-list"
-
-            );
-
-
-
-        if(!area){
-
-
-            return;
-
-
-        }
-
-
-
-        area.innerHTML = "";
-
-
-
-        const data =
-
-            ResearchManager.getAll();
-
-
-
-        for(
-
-            const id in data
+            this.level >= this.max
 
         ){
 
 
-            const research =
-
-                data[id];
-
-
-
-            const button =
-
-                document.createElement(
-
-                    "button"
-
-                );
-
-
-
-            button.textContent =
-
-
-                research.name
-
-                +
-
-                " "
-
-                +
-
-                (
-
-                    research.unlocked
-
-                    ?
-
-                    "Unlocked"
-
-                    :
-
-                    "Cost "
-
-                    +
-
-                    research.cost
-
-                );
-
-
-
-            button.disabled =
-
-                research.unlocked;
-
-
-
-            button.onclick = () => {
-
-
-                if(
-
-                    ResearchManager.unlock(
-
-                        id
-
-                    )
-
-                ){
-
-
-                    this.update();
-
-
-                }
-
-
-            };
-
-
-
-            area.appendChild(
-
-                button
-
-            );
-
+            return;
 
         }
 
 
+
+        this.level++;
+
+
+
+        this.progress=
+
+        BigNumber.zero();
+
+
+
+        this.completed=true;
+
+
     }
+
+
+
+    getMultiplier(){
+
+
+        return Math.pow(
+
+            this.effect,
+
+            this.level
+
+        );
+
+
+    }
+
+
+
+    toJSON(){
+
+
+        return {
+
+
+            id:
+
+            this.id,
+
+
+            name:
+
+            this.name,
+
+
+            level:
+
+            this.level,
+
+
+            max:
+
+            this.max,
+
+
+            effect:
+
+            this.effect,
+
+
+            type:
+
+            this.type,
+
+
+            cost:
+
+            this.cost,
+
+
+            progress:
+
+            this.progress.toJSON()
+
+
+
+        };
+
+
+    }
+
+
+
+    load(data){
+
+
+        if(!data){
+
+            return;
+
+        }
+
+
+
+        this.level=
+
+        data.level
+
+        ??
+
+        0;
+
+
+
+        this.progress=
+
+        BigNumber.from(
+
+            data.progress
+
+        );
+
+
+    }
+
 
 
 }
 
 
 
-const researchUI =
-
-    new ResearchUI();
-
-
-
-export default researchUI;
+export default Research;
