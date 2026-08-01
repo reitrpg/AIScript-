9776b0162ce6c332c665360107a9acd0192d859e
reitrpg <rei.you2.11@gmail.com>
2026-08-01 09:15:27 +0900

    Converter.js の更新

--- js/resource/Converter.js
+++ js/resource/Converter.js
@@ -1,262 +1,105 @@
-/**
- * World Creator
- * Resource Converter
- *
- * 資源変換・生産システム
- */
-
-
 import ResourceManager from "./Manager.js";
 
-import eventBus from "../core/eventBus.js";
 
+class Converter{
 
 
-class Converter {
+constructor(){
 
+this.recipes={};
 
-    constructor() {
 
+this.register();
 
-        this.recipes = {};
+}
 
 
-    }
 
+register(){
 
+this.recipes={
 
-    /**
-     * レシピ登録
-     */
 
-    register(
-        id,
-        input,
-        output
-    ) {
+food:{
 
+input:"wood",
 
-        this.recipes[id] = {
+cost:2,
 
+output:1
 
-            input,
+},
 
-            output
 
+mana:{
 
-        };
+input:"stone",
 
+cost:5,
 
-    }
+output:1
 
+}
 
 
-    /**
-     * 変換可能確認
-     */
-
-    canConvert(id) {
-
-
-        const recipe =
-
-            this.recipes[id];
-
-
-
-        if (!recipe) {
-
-            return false;
-
-        }
-
-
-
-        for (
-            const key in recipe.input
-        ) {
-
-
-            const resource =
-
-                ResourceManager.get(
-                    key
-                );
-
-
-
-            if (
-
-                resource
-                    .getValue()
-                    .compare(
-
-                        recipe.input[key]
-
-                    )
-
-                    <
-
-                    0
-
-            ) {
-
-
-                return false;
-
-
-            }
-
-
-        }
-
-
-
-        return true;
-
-
-    }
-
-
-
-    /**
-     * 変換実行
-     */
-
-    convert(id) {
-
-
-        if (
-            !this.canConvert(id)
-        ) {
-
-
-            return false;
-
-
-        }
-
-
-
-        const recipe =
-
-            this.recipes[id];
-
-
-
-        for (
-            const key in recipe.input
-        ) {
-
-
-            ResourceManager.remove(
-
-                key,
-
-                recipe.input[key]
-
-            );
-
-
-        }
-
-
-
-        for (
-            const key in recipe.output
-        ) {
-
-
-            ResourceManager.add(
-
-                key,
-
-                recipe.output[key]
-
-            );
-
-
-        }
-
-
-
-        eventBus.emit(
-
-            "resource:convert",
-
-            id
-
-        );
-
-
-
-        return true;
-
-
-    }
-
-
-
-    /**
-     * Tick処理
-     */
-
-    tick() {
-
-
-        for (
-            const id in this.recipes
-        ) {
-
-
-            this.convert(
-                id
-            );
-
-
-        }
-
-
-    }
+};
 
 
 }
 
 
 
-const converter =
+tick(){
 
-    new Converter();
+for(
+const id in this.recipes
+){
+
+const recipe=
+
+this.recipes[id];
 
 
 
-/*
- 初期レシピ
-*/
+const input=
 
-
-converter.register(
-
-    "wood_to_food",
-
-    {
-
-        wood:
-
-            10
-
-    },
-
-    {
-
-        food:
-
-            5
-
-    }
-
+ResourceManager.get(
+recipe.input
 );
 
+
+
+if(
+input.amount>=recipe.cost
+){
+
+input.remove(
+recipe.cost
+);
+
+
+ResourceManager.add(
+id,
+recipe.output
+);
+
+
+}
+
+}
+
+
+}
+
+
+
+}
+
+
+
+const converter=
+
+new Converter();
 
 
 export default converter;
