f7c41f07d0d350d31eefb6c1bda6d57ef5c01d2c
reitrpg <rei.you2.11@gmail.com>
2026-08-01 11:07:02 +0900

    BigNumber.js の更新

--- js/number/BigNumber.js
+++ js/number/BigNumber.js
@@ -1,56 +1,341 @@
-class BigNumber{
+/**
+ * World Creator
+ * Big Number System
+ */
 
 
-constructor(value=0){
-
-this.value=Number(value);
-
-}
+class BigNumber {
 
 
+    constructor(
 
-add(value){
+        value=0,
 
-this.value+=Number(value);
+        exponent=0
 
-}
+    ){
 
 
+        this.value=
 
-subtract(value){
-
-this.value-=Number(value);
-
-}
+        Number(value);
 
 
 
-toString(){
+        this.exponent=
 
-return this.value.toString();
-
-}
+        Number(exponent);
 
 
 
-toJSON(){
-
-return this.value;
-
-}
+        this.normalize();
 
 
-
-static from(value){
-
-return new BigNumber(value);
-
-}
+    }
 
 
 
-}
+    static zero(){
+
+
+        return new BigNumber(
+
+            0,
+
+            0
+
+        );
+
+
+    }
 
 
 
-export default BigNumber;
+    static from(data){
+
+
+        if(
+
+            data instanceof BigNumber
+
+        ){
+
+            return data;
+
+        }
+
+
+
+        if(
+
+            typeof data==="number"
+
+        ){
+
+            return new BigNumber(
+
+                data,
+
+                0
+
+            );
+
+        }
+
+
+
+        if(
+
+            !data
+
+        ){
+
+            return BigNumber.zero();
+
+        }
+
+
+
+        return new BigNumber(
+
+            data.value ?? 0,
+
+            data.exponent ?? 0
+
+        );
+
+
+    }
+
+
+
+    normalize(){
+
+
+        if(
+
+            this.value===0
+
+        ){
+
+            this.exponent=0;
+
+            return;
+
+        }
+
+
+
+        while(
+
+            Math.abs(this.value)>=10
+
+        ){
+
+
+            this.value/=10;
+
+
+            this.exponent++;
+
+
+        }
+
+
+
+        while(
+
+            Math.abs(this.value)<1
+
+            &&
+
+            this.value!==0
+
+        ){
+
+
+            this.value*=10;
+
+
+            this.exponent--;
+
+
+        }
+
+
+    }
+
+
+
+    add(other){
+
+
+        const num=
+
+        BigNumber.from(
+
+            other
+
+        );
+
+
+
+        if(
+
+            this.exponent===num.exponent
+
+        ){
+
+
+            this.value+=num.value;
+
+
+        }
+
+        else{
+
+
+            const diff=
+
+            this.exponent-num.exponent;
+
+
+
+            if(
+
+                diff>=0
+
+            ){
+
+
+                this.value+=
+
+                num.value
+
+                *
+
+                Math.pow(
+
+                    10,
+
+                    -diff
+
+                );
+
+
+            }
+
+            else{
+
+
+                this.value=
+
+                this.value
+
+                *
+
+                Math.pow(
+
+                    10,
+
+                    diff
+
+                )
+
+                +
+
+                num.value;
+
+
+
+                this.exponent=
+
+                num.exponent;
+
+
+            }
+
+
+        }
+
+
+
+        this.normalize();
+
+
+
+        return this;
+
+
+    }
+
+
+
+    compare(other){
+
+
+        const num=
+
+        BigNumber.from(
+
+            other
+
+        );
+
+
+
+        if(
+
+            this.exponent!==num.exponent
+
+        ){
+
+
+            return this.exponent-num.exponent;
+
+
+        }
+
+
+
+        return this.value-num.value;
+
+
+    }
+
+
+
+    greaterOrEqual(other){
+
+
+        return (
+
+            this.compare(
+
+                other
+
+            )
+
+            >=0
+
+        );
+
+
+    }
+
+
+
+    toJSON(){
+
+
+        return {
+
+
+            value:
+
+            this.value,
+
+
+            exponent:
+
+            this
