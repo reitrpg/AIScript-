204cf0a2ebf34ca7415b3c43bace5a61d3832c65
reitrpg <rei.you2.11@gmail.com>
2026-08-01 10:02:54 +0900

    Manager.js の更新

--- js/world/Manager.js
+++ js/world/Manager.js
@@ -1,51 +1,85 @@
 /**
  * World Creator
- * World Growth System
+ * World Level System
  *
- * Level Event Integration
+ * Exponential EXP Curve
  */
 
 
-import eventBus from "../core/eventBus.js";
+getNeedExp(){
 
 
+    if(!this.current){
 
-class WorldManager {
-
-
-    constructor(){
-
-
-        this.current=null;
-
-
-        this.unlocks={
-
-            extraResource1:false,
-
-            extraResource2:false,
-
-            rareResource:false
-
-        };
-
+        return 100;
 
     }
 
 
 
-    levelUp(){
+    return Math.floor(
 
 
-        if(!this.current){
+        100 *
 
-            return;
+        Math.pow(
 
-        }
+            1.35,
+
+            this.current.level - 1
+
+        )
+
+
+    );
+
+
+}
+
+
+
+addExp(value){
+
+
+    if(!this.current){
+
+        return;
+
+    }
+
+
+
+    this.current.exp += value;
+
+
+
+    let levelUp=false;
+
+
+
+    while(
+
+
+        this.current.exp >=
+
+        this.getNeedExp()
+
+
+    ){
+
+
+
+        this.current.exp -=
+
+        this.getNeedExp();
 
 
 
         this.current.level++;
+
+
+
+        levelUp=true;
 
 
 
@@ -62,6 +96,12 @@ class WorldManager {
         );
 
 
+    }
+
+
+
+    if(levelUp){
+
 
         eventBus.emit(
 
@@ -75,115 +115,4 @@ class WorldManager {
     }
 
 
-
-    checkUnlock(){
-
-
-        const lv=
-
-        this.current.level;
-
-
-
-        if(
-
-            lv>=10
-
-            &&
-
-            !this.unlocks.extraResource1
-
-        ){
-
-
-            this.unlocks.extraResource1=true;
-
-
-
-            eventBus.emit(
-
-                "world:unlock",
-
-                "resource_slot"
-
-            );
-
-
-        }
-
-
-
-        if(
-
-            lv>=50
-
-            &&
-
-            !this.unlocks.extraResource2
-
-        ){
-
-
-            this.unlocks.extraResource2=true;
-
-
-
-            eventBus.emit(
-
-                "world:unlock",
-
-                "advanced_resource"
-
-            );
-
-
-        }
-
-
-
-        if(
-
-            lv>=100
-
-            &&
-
-            !this.unlocks.rareResource
-
-        ){
-
-
-            this.unlocks.rareResource=true;
-
-
-
-            eventBus.emit(
-
-                "world:unlock",
-
-                "rare_material"
-
-            );
-
-
-        }
-
-
-    }
-
-
-
-    getUnlocks(){
-
-
-        return this.unlocks;
-
-
-    }
-
-
-
 }
-
-
-
-export default new WorldManager();
