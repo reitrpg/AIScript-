69c5a47e979f6724fc11d3dd7a0faf1d77c2ecf1
reitrpg <rei.you2.11@gmail.com>
2026-08-01 09:27:27 +0900

    time.js の更新

--- js/core/time.js
+++ js/core/time.js
@@ -1,105 +1,164 @@
+/**
+ * World Creator
+ * Time Manager
+ *
+ * Integrated Version
+ */
+
+
 import eventBus from "./eventBus.js";
 
 
-class TimeManager{
+
+class TimeManager {
 
 
-constructor(){
+    constructor(){
 
-this.interval=null;
 
-this.tick=0;
+        this.interval = null;
 
-this.running=false;
+
+        this.tick = 0;
+
+
+        this.running = false;
+
+
+    }
+
+
+
+    start(){
+
+
+        if(this.running){
+
+
+            return;
+
+
+        }
+
+
+
+        this.running = true;
+
+
+
+        this.interval = setInterval(
+
+            ()=>{
+
+
+                this.tick++;
+
+
+
+                eventBus.emit(
+
+                    "time:tick",
+
+                    this.tick
+
+                );
+
+
+            },
+
+            1000
+
+        );
+
+
+    }
+
+
+
+    stop(){
+
+
+        if(this.interval){
+
+
+            clearInterval(
+
+                this.interval
+
+            );
+
+
+            this.interval = null;
+
+
+        }
+
+
+
+        this.running = false;
+
+
+    }
+
+
+
+    getTick(){
+
+
+        return this.tick;
+
+
+    }
+
+
+
+    toJSON(){
+
+
+        return {
+
+
+            tick:
+
+                this.tick
+
+
+        };
+
+
+    }
+
+
+
+    load(data){
+
+
+        if(!data){
+
+
+            return;
+
+
+        }
+
+
+
+        this.tick =
+
+            data.tick ?? 0;
+
+
+    }
+
 
 }
 
 
 
-start(){
+const time =
 
-if(this.running){
+    new TimeManager();
 
-return;
-
-}
-
-
-this.running=true;
-
-
-this.interval=setInterval(
-
-()=>{
-
-this.tick++;
-
-eventBus.emit(
-"time:tick",
-this.tick
-);
-
-
-},
-
-1000
-
-);
-
-
-}
-
-
-
-stop(){
-
-this.running=false;
-
-
-if(this.interval){
-
-clearInterval(
-this.interval
-);
-
-this.interval=null;
-
-}
-
-
-}
-
-
-
-toJSON(){
-
-return{
-
-tick:this.tick
-
-};
-
-}
-
-
-
-load(data){
-
-if(data){
-
-this.tick=data.tick ?? 0;
-
-}
-
-}
-
-
-
-}
-
-
-
-const time=new TimeManager();
 
 
 export default time;
