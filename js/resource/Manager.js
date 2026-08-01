f0a26c6c87d1467b6806e4957492c2064287e1d6
reitrpg <rei.you2.11@gmail.com>
2026-08-01 09:25:40 +0900

    Manager.js の更新

--- js/resource/Manager.js
+++ js/resource/Manager.js
@@ -1,188 +1,350 @@
+/**
+ * World Creator
+ * Resource Manager
+ *
+ * Integrated Version
+ */
+
+
 import Resource from "./Resource.js";
 
+import eventBus from "../core/eventBus.js";
 
-class ResourceManager{
 
 
-constructor(){
+class ResourceManager {
 
-this.resources={};
 
-this.initialized=false;
+    constructor(){
+
+
+        this.resources = {};
+
+
+        this.initialized = false;
+
+
+    }
+
+
+
+    init(){
+
+
+        if(this.initialized){
+
+
+            return;
+
+
+        }
+
+
+
+        this.create(
+
+            "wood",
+
+            "Wood",
+
+            1
+
+        );
+
+
+
+        this.create(
+
+            "stone",
+
+            "Stone",
+
+            1
+
+        );
+
+
+
+        this.create(
+
+            "food",
+
+            "Food",
+
+            0
+
+        );
+
+
+
+        this.create(
+
+            "mana",
+
+            "Mana",
+
+            0
+
+        );
+
+
+
+        this.initialized = true;
+
+
+    }
+
+
+
+    create(
+
+        id,
+
+        name,
+
+        production
+
+    ){
+
+
+        this.resources[id] =
+
+            new Resource(
+
+                id,
+
+                name
+
+            );
+
+
+
+        this.resources[id]
+
+        .setProduction(
+
+            production
+
+        );
+
+
+    }
+
+
+
+    add(
+
+        id,
+
+        value
+
+    ){
+
+
+        const resource =
+
+            this.resources[id];
+
+
+
+        if(!resource){
+
+
+            return;
+
+
+        }
+
+
+
+        resource.add(
+
+            value
+
+        );
+
+
+    }
+
+
+
+    remove(
+
+        id,
+
+        value
+
+    ){
+
+
+        const resource =
+
+            this.resources[id];
+
+
+
+        if(!resource){
+
+
+            return false;
+
+
+        }
+
+
+
+        return resource.remove(
+
+            value
+
+        );
+
+
+    }
+
+
+
+    get(
+
+        id
+
+    ){
+
+
+        return this.resources[id];
+
+
+    }
+
+
+
+    getAll(){
+
+
+        return this.resources;
+
+
+    }
+
+
+
+    update(){
+
+
+        Object.values(
+
+            this.resources
+
+        )
+
+        .forEach(
+
+            resource => {
+
+
+                resource.update();
+
+
+            }
+
+        );
+
+
+
+        eventBus.emit(
+
+            "resource:update",
+
+            this.resources
+
+        );
+
+
+    }
+
+
+
+    toJSON(){
+
+
+        const data = {};
+
+
+
+        for(
+
+            const id in this.resources
+
+        ){
+
+
+            data[id] =
+
+                this.resources[id]
+
+                .toJSON();
+
+
+        }
+
+
+
+        return data;
+
+
+    }
+
+
+
+    load(data){
+
+
+        if(!data){
+
+
+            return;
+
+
+        }
+
+
+
+        for(
+
+            const id in data
+
+        ){
+
+
+            if(this.resources[id]){
+
+
+                this.resources[id]
+
+                .load(
+
+                    data[id]
+
+                );
+
+
+            }
+
+
+        }
+
+
+    }
 
 
 }
 
 
 
-init(){
+const resourceManager =
 
-if(this.initialized){
+    new ResourceManager();
 
-return;
 
-}
 
-
-this.create(
-"wood",
-"Wood"
-);
-
-
-this.create(
-"stone",
-"Stone"
-);
-
-
-this.create(
-"food",
-"Food"
-);
-
-
-this.create(
-"mana",
-"Mana"
-);
-
-
-
-this.resources.wood
-.setProduction(1);
-
-
-this.resources.stone
-.setProduction(1);
-
-
-
-this.initialized=true;
-
-
-}
-
-
-
-create(id,name){
-
-this.resources[id]=
-
-new Resource(
-id,
-name
-);
-
-}
-
-
-
-add(id,value){
-
-if(this.resources[id]){
-
-this.resources[id]
-.add(value);
-
-}
-
-}
-
-
-
-get(id){
-
-return this.resources[id];
-
-}
-
-
-
-getAll(){
-
-return this.resources;
-
-}
-
-
-
-update(){
-
-Object.values(
-this.resources
-)
-.forEach(
-
-resource=>
-resource.update()
-
-);
-
-
-}
-
-
-
-toJSON(){
-
-const data={};
-
-
-for(
-const id in this.resources
-){
-
-data[id]=
-
-this.resources[id]
-.toJSON();
-
-}
-
-
-return data;
-
-}
-
-
-
-load(data){
-
-if(!data){
-
-return;
-
-}
-
-
-for(
-const id in data
-){
-
-if(this.resources[id]){
-
-this.resources[id]
-.load(
-data[id]
-);
-
-}
-
-}
-
-
-}
-
-
-
-}
-
-
-
-const manager=
-
-new ResourceManager();
-
-
-export default manager;
+export default resourceManager;
